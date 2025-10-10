2dec95869c67b22a8e70e8a16e3524e5b1240627
Danylo Solokha <nolaxlive@gmail.com>
2025-10-10 12:49:14 +0300

    Preparation for working with db: -create empty model classes and DTO classes -create empty repository classes -ignor .db files

---
+++ EF/DbDependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF
+{
+    public static class DbDependencyInjection
+    {
+        public static IServiceCollection Add(this IServiceCollection services, string connectionString)
+        {
+            services.AddDbContext<MyDbContext>(options => options.UseSqlite(connectionString));
+
+            return services;
+        }
+    }
+}
