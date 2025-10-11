c62b555d37e5a5ae506a09ae982ab60467ef9998
Danylo Solokha <nolaxlive@gmail.com>
2025-10-11 19:12:58 +0300

    add AccountDto and AccountController with blank endpoints

---
+++ WEBAPI/Controllers/AccountController.cs
@@ -0,0 +1,43 @@
+﻿using ENTITIES.DTOs;
+using ENTITIES.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WEBAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AccountController : MyController
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+        private readonly IConfiguration _config;
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager, IConfiguration config)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _config = config;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(AccountDto account)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(AccountDto account)
+        {
+            throw new NotImplementedException();
+        }
+
+        [Authorize]
+        [HttpPost("change-password")]
+        public async Task<IActionResult> ChangePassword(AccountDto account)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WEBAPI/Controllers/MyController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WEBAPI.Controllers
+{
+    public class MyController : Controller
+    {
+
+    }
+}
