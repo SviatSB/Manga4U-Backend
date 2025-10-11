d6dbae06553cd38fcd533c570c2af437cbbc602b
Danylo Solokha <nolaxlive@gmail.com>
2025-10-11 14:38:44 +0300

    add missing references to comments model

--- ENTITIES/Models/Comment.cs
+++ ENTITIES/Models/Comment.cs
@@ -11,20 +11,22 @@ namespace ENTITIES.Models
         public ulong Id { get; set; }
         public DateTime CreationTime { get; set; } = DateTime.Now; //not null | has default
         public string Text { get; set; } = null!; //not null
-        public int UserId { get; set; }  // FK User
-        public int MangaId { get; set; }  // FK Manga
+
+
 
         //FK and References
+        public int UserId { get; set; }  // FK User
+        public int MangaId { get; set; }  // FK Manga
+        public User User { get; set; }
+        public Manga Manga { get; set; }
+
+        //на который отвечают
+        public int? RepliedCommentId { get; set; }  // FK Comment (DELETE CASCADE)
+        public Comment? RepliedComment { get; set; }
+
+        //которыми ответили на этот
+        public ICollection<Comment> RepliesComments { get; set; } = new List<Comment>();
 
 
-        //RepliedCommentId
-
-        //public int? RepliedCommentId { get; set; }  // FK Comment (DELETE CASCADE)
-        //public int UserId { get; set; }  // FK User
-        //public int MangaId { get; set; }  // FK Manga
-
-        // public Comment? RepliedComment { get; set; }
-        // public User User { get; set; }
-        // public Manga Manga { get; set; }
     }
 }
