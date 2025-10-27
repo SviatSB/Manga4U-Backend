edeb5b0163a733bc63da94d4dbb852a5b607ad2a
Danylo Solokha <nolaxlive@gmail.com>
2025-10-27 23:57:09 +0200

    allow all origin

--- WEBAPI/Program.cs
+++ WEBAPI/Program.cs
@@ -113,12 +113,7 @@ namespace WEBAPI
                 options.AddPolicy(FrontendCorsPolicy, policy =>
                 {
                     policy
-                        .WithOrigins(
-                            "http://127.0.0.1:5500",
-                            "https://127.0.0.1:5500",
-                            "http://localhost:5500",
-                            "https://localhost:5500"
-                        )
+                        .AllowAnyOrigin()
                         .AllowAnyHeader()
                         .AllowAnyMethod()
                         .AllowCredentials();
