49a96f9e4c64c11d70c6c44c98042a8bde5548de
Danylo Solokha <nolaxlive@gmail.com>
2025-12-01 20:17:11 +0200

    -change comments model -create new migration for sqlite

--- Domain/Models/Comment.cs
+++ Domain/Models/Comment.cs
@@ -4,14 +4,12 @@
     {
         public DateTime CreationTime { get; set; } = DateTime.Now; //not null | has default
         public string Text { get; set; } = null!; //not null
-
+        public string ChapterExternalId { get; set; } = null!; //not null
 
 
         //FK and References
         public long UserId { get; set; }  // FK User
-        public long MangaId { get; set; }  // FK Manga
         public User User { get; set; } = null!;
-        public Manga Manga { get; set; } = null!;
 
         //на который отвечают
         public long? RepliedCommentId { get; set; }  // FK Comment (DELETE CASCADE)
---
+++ SqliteMigrations/Migrations/20251201180906_comments1.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SqliteMigrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class comments1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_Mangas_MangaId",
+                table: "Comments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Comments_MangaId",
+                table: "Comments");
+
+            migrationBuilder.DropColumn(
+                name: "MangaId",
+                table: "Comments");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ChapterExternalId",
+                table: "Comments",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ChapterExternalId",
+                table: "Comments");
+
+            migrationBuilder.AddColumn<long>(
+                name: "MangaId",
+                table: "Comments",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_MangaId",
+                table: "Comments",
+                column: "MangaId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_Mangas_MangaId",
+                table: "Comments",
+                column: "MangaId",
+                principalTable: "Mangas",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
