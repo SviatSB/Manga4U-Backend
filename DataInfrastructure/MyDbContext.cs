65d825cf8dad8c6378cf98714e32422a31df7064
Danylo Solokha <nolaxlive@gmail.com>
2025-12-01 22:25:52 +0200

    -set uniqness for review -set cascade delete behavior for comments -add new migrations for sqlite

--- DataInfrastructure/MyDbContext.cs
+++ DataInfrastructure/MyDbContext.cs
@@ -19,5 +19,28 @@ namespace DataInfrastructure
         public DbSet<Tag> Tags { get; set; }
         public DbSet<Review> Reviews { get; set; }
         public DbSet<Comment> Comments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Comment>()
+                .HasOne(c => c.RepliedComment)
+                .WithMany(c => c.RepliesComments)
+                .HasForeignKey(c => c.RepliedCommentId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<Review>()
+                .HasIndex(r => new { r.UserId, r.MangaId })
+                .IsUnique();
+
+            //builder.Entity<Collection>(builder =>
+            //{
+            //    builder.HasIndex(c => new { c.UserId, c.MangaId })
+            //        .IsUnique();
+
+            //    builder.Property(c => c.);
+            //});
+        }
     }
 }
---
+++ SqliteMigrations/Migrations/20251201195140_uniqReview.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SqliteMigrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class uniqReview : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Reviews_UserId",
+                table: "Reviews");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_UserId_MangaId",
+                table: "Reviews",
+                columns: new[] { "UserId", "MangaId" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Reviews_UserId_MangaId",
+                table: "Reviews");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_UserId",
+                table: "Reviews",
+                column: "UserId");
+        }
+    }
+}
